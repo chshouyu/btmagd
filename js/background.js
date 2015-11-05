efd85ec5b68797be0a155f44003a49926fa568b1
chshouyu <chshouyu@126.com>
2015-11-05 12:25:24 +0800

    add context menu support

--- js/background.js
+++ js/background.js
@@ -1,7 +1,42 @@
+var appUrl = chrome.extension.getURL('../app/index.html');
+
+var appTabId = null;
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-    var appUrl = chrome.extension.getURL('../app/index.html');
     chrome.tabs.create({
         url: appUrl
+    }, function(tab) {
+        appTabId = tab.id;
     });
+});
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+
+    if (info.menuItemId === 'bt2mag') {
+        var openUrl = appUrl + '#/' + encodeURIComponent(info.selectionText);
+        if (appTabId !== null) {
+            chrome.tabs.update(appTabId, {
+                url: openUrl,
+                active: true
+            });
+        } else {
+            chrome.tabs.create({
+                url: openUrl
+            }, function(tab) {
+                appTabId = tab.id;
+            });
+        }
+    }
+});
+
+chrome.tabs.onRemoved.addListener(function(tabId) {
+    if (tabId === appTabId) {
+        appTabId = null;
+    }
+});
+
+chrome.contextMenus.create({
+    "title": "使用bt2mag搜索 \"%s\"",
+    "contexts": ["selection"],
+    "id": "bt2mag"
 });
