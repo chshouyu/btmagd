0a0af1168ad5689589c4ec25172667632916546a
chshouyu <chshouyu@126.com>
2016-01-18 23:17:49 +0800

    do search when click page number

--- js/background.js
+++ js/background.js
@@ -2,6 +2,22 @@ var appUrl = chrome.extension.getURL('../app/dest/index.html');
 
 var appTabId = null;
 
+function doSearch(number) {
+    var openUrl = appUrl + '#/' + encodeURIComponent(number);
+    if (appTabId !== null) {
+        chrome.tabs.update(appTabId, {
+            url: openUrl,
+            active: true
+        });
+    } else {
+        chrome.tabs.create({
+            url: openUrl
+        }, function(tab) {
+            appTabId = tab.id;
+        });
+    }
+}
+
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.create({
         url: appUrl
@@ -11,21 +27,8 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 });
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
-
     if (info.menuItemId === 'btmagd') {
-        var openUrl = appUrl + '#/' + encodeURIComponent(info.selectionText);
-        if (appTabId !== null) {
-            chrome.tabs.update(appTabId, {
-                url: openUrl,
-                active: true
-            });
-        } else {
-            chrome.tabs.create({
-                url: openUrl
-            }, function(tab) {
-                appTabId = tab.id;
-            });
-        }
+        doSearch(info.selectionText);
     }
 });
 
@@ -39,4 +42,10 @@ chrome.contextMenus.create({
     "title": "使用btmagd搜索 \"%s\"",
     "contexts": ["selection"],
     "id": "btmagd"
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.number) {
+        doSearch(request.number);
+    }
 });
---
+++ js/content.js
@@ -0,0 +1,16 @@
+
+var host = window.location.host;
+
+function openSearch(e) {
+    e.preventDefault();
+    chrome.runtime.sendMessage({
+        number: this.textContent.trim()
+    }, function(response) {});
+}
+
+var selector = /javmoo/.test(host) ? '.movie .info p span:nth-child(2)' : '#video_id tbody td:nth-child(2)';
+
+var node = document.querySelector(selector);
+
+node.style.cursor = 'pointer';
+node.addEventListener('click', openSearch, false);
