7c7ef36278c30153a7bc696f42425e22a1565f99
chshouyu <chshouyu@126.com>
2016-02-20 22:22:31 +0800

    update avmoo url

--- js/content.js
+++ js/content.js
@@ -8,7 +8,7 @@ function openSearch(e) {
     }, function(response) {});
 }
 
-var selector = /javmoo/.test(host) ? '.movie .info p span:nth-child(2)' : '#video_id tbody td:nth-child(2)';
+var selector = /avmoo/.test(host) ? '.movie .info p span:nth-child(2)' : '#video_id tbody td:nth-child(2)';
 
 var node = document.querySelector(selector);
 
